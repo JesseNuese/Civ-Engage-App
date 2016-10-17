ee2e2d7139d1ce3b2d7d283ce858883dda68a381
Student <student@Mac-mini-7.local>
2016-10-17 11:39:47 -0600

    added carousel

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
 // Conroller & API
-angular.module('civapp',[])
-  .controller('civCtrl', civController);
+angular.module('civapp', [])
+    .controller('civCtrl', civController);
 
-  civController.$inject=['$http'];
+civController.$inject = ['$http'];
 
-  function civController($http) {
+function civController($http) {
 
     var civ = this;
-     civ.getInfo = function (){
-       $http.get('https://www.googleapis.com/civicinfo/v2/representatives?address=4631StarboardDr&key=AIzaSyC9LRRpJaqbHq1_wqecCOeSX0wFIKf14T4')
-       .then(function(res, status){
-        civ.myData = res.data;
-      },
-      function(res, status){
-        console.log('Failure', status);
-      });
+    civ.getInfo = function() {
+        $http.get('https://www.googleapis.com/civicinfo/v2/representatives?address=4631StarboardDr&key=AIzaSyC9LRRpJaqbHq1_wqecCOeSX0wFIKf14T4')
+            .then(function(res, status) {
+                    civ.myData = res.data;
+                },
+                function(res, status) {
+                    console.log('Failure', status);
+                });
     }
     civ.getInfo();
-  }
+}
 
 // Object with API data
 
-var evs = function(){
+var evs = function() {
 
 }
