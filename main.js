8a9fda6085348b1c144bd70a41c6a1445cbb6198
Student <student@Mac-mini-7.local>
2016-10-16 18:12:16 -0600

    The Packers lost

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 angular.module('civapp',[])
   .controller('civCtrl', civController);
 
-  civController.$inject['$http'];
+  civController.$inject=['$http'];
 
-  function civController() {
+  function civController($http) {
 
     var civ = this;
-    civ.text = "Hello";
+     civ.getInfo = function (){
+       $http.get('https://www.googleapis.com/civicinfo/v2/voterinfo?address=4631StarboardDr&key=insertkeyhere')
+       .then(function(res, status){
+        civ.myCiv = res.data;
+      });
+    }
+    civ.getInfo();
   }
