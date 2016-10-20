bbce7baf53b0aeeb78c22623db48bba67385dbf7
Student <student@Mac-mini-7.local>
2016-10-20 13:08:54 -0600

    Whats better than being cool?

--- controller/mainCtrl.js
+++ controller/mainCtrl.js
@@ -7,18 +7,19 @@ civController.$inject = ['$http'];
 function civController($http) {
     var civ = this;
     civ.show = false;
+    civ.officeOfficals = [];
     civ.getInfo = function() {
         $http({
-          method: 'GET',
-          url: 'https://www.googleapis.com/civicinfo/v2/voterinfo',
-          params: {
-            key:'IKH',
-            address: civ.searchQuery
-          }
-        })
+                method: 'GET',
+                url: 'https://www.googleapis.com/civicinfo/v2/voterinfo',
+                params: {
+                    key: 'AIKH',
+                    address: civ.searchQuery
+                }
+            })
             .then(function(res, status) {
                     civ.myData = res.data;
-                    console.log(civ.myData.pollingLocations)
+                    console.log(civ.myData)
                     civ.pollArray = civ.myData.pollingLocations;
                 },
                 function(res, status) {
@@ -26,20 +27,31 @@ function civController($http) {
                 });
     }
     civ.getReps = function() {
-      $http({
-      method: 'GET',
-      url: 'https://www.googleapis.com/civicinfo/v2/representatives',
-      params: {
-        key:'IKH',
-        address: civ.repQuery
-      }
-      })
-      .then(function(res, status){
-        civ.myReps = res.data;
-        console.log(civ.myReps)
-      },
-      function(res, status) {
-        console.log('Failure', status);
-      })
+        $http({
+                method: 'GET',
+                url: 'https://www.googleapis.com/civicinfo/v2/representatives',
+                params: {
+                    key: 'AIKH',
+                    address: civ.repQuery,
+                    levels: 'country'
+                }
+            })
+            .then(function(res, status) {
+                    civ.myReps = res.data;
+                    console.log(civ.myReps)
+                    civ.repArray = civ.myReps;
+
+                    civ.myReps.offices.forEach(function(office, index){
+                      civ.officeOfficals.push({
+                        officeName: office.name,
+                        officialName: civ.myReps.officials[index].name,
+                        officialParty: civ.myReps.officials[index].party,
+                        officialPhoto: civ.myReps.officials[index].photoUrl
+                      });
+                    })
+                },
+                function(res, status) {
+                    console.log('Failure', status);
+                })
     };
-    }
+}
